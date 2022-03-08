d2840221e19c2d2cd3d7f2e591090b96449b6c2a
danil <danil.vladimirow2016@yandex.ru>
2022-03-08 19:25:52 +0300

    1 commit

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyChnnarm')
+    print_hi('PyChnnar,,,,,,,,,,,m')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
