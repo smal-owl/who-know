a2454e132cd68fab356d63cbc9d5b51647476e7f
Egor <kazenovegoric@gmail.com>
2022-03-08 20:32:16 +0300

    Сделал начальный шаблон шапки

--- main.py
+++ main.py
@@ -2,6 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def login():
-    return render_template('login.html')
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
