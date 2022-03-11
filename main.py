7bbd95abb9918f20e583c2c2fc5f5028309522d0
Sampy <kazenovegoric@gmail.com>
2022-03-11 19:11:08 +0300

    Добавил папку для базы данных

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template
+from flask_login import LoginManager
 
 app = Flask(__name__)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 @app.route('/')
 def main_window():
     return render_template('index.html')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
 
 
 if __name__ == '__main__':
