f2886666f9910d2c5a2347057fa6091a1b7ef0de
Sampy <kazenovegoric@gmail.com>
2022-03-11 19:52:43 +0300

    Шаблон для регистрации.

--- data/users.py
+++ data/users.py
@@ -2,6 +2,8 @@ import datetime
 import sqlalchemy
 from sqlalchemy import orm
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
 from .db_session import SqlAlchemyBase
 
 
@@ -19,3 +21,9 @@ class User(SqlAlchemyBase, UserMixin):
                                      default=datetime.datetime.now)
 
     news = orm.relation("News", back_populates='user')
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from flask_login import LoginManager
+from werkzeug.utils import redirect
+
 from data import db_session
 from data.users import User
 from data.news import News
+from forms.user import RegisterForm
 
 app = Flask(__name__)
 
@@ -37,6 +40,31 @@ def main_window():
     return render_template('index.html')
 
 
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db_session.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
 @login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
@@ -46,5 +74,4 @@ def load_user(user_id):
 
 if __name__ == '__main__':
     db_session.global_init("db/blogs.db")
-    add_news()
     app.run(port=8080, host='127.0.0.1')
