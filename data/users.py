4b03713a51c5ffaeaf8bfa7a1f96ae44fc831097
Sampy <kazenovegoric@gmail.com>
2022-03-11 19:42:55 +0300

    Добавил классы User и News для дальнейшей регистрации пользоватея.

---
+++ data/users.py
@@ -0,0 +1,21 @@
+import datetime
+import sqlalchemy
+from sqlalchemy import orm
+from flask_login import UserMixin
+from .db_session import SqlAlchemyBase
+
+
+class User(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    about = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String,
+                              index=True, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+
+    news = orm.relation("News", back_populates='user')
--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 from flask import Flask, render_template
 from flask_login import LoginManager
+from data import db_session
+from data.users import User
+from data.news import News
 
 app = Flask(__name__)
 
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
 login_manager = LoginManager()
 login_manager.init_app(app)
+
+
+def add_user():
+    user = User()
+    user.name = "Пользователь 1"
+    user.about = "биография пользователя 1"
+    user.email = "fls;ds@gmail.com"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
+
+
+def add_news():
+    news = News()
+    news.title = "Пользователь 1"
+    news.content = "биография пользователя 1"
+    news.is_private = False
+    db_sess = db_session.create_session()
+    db_sess.add(news)
+    db_sess.commit()
 
 
 @app.route('/')
@@ -15,8 +40,11 @@ def main_window():
 @login_manager.user_loader
 def load_user(user_id):
     db_sess = db_session.create_session()
+    user = db_sess.query(User).all()
     return db_sess.query(User).get(user_id)
 
 
 if __name__ == '__main__':
+    db_session.global_init("db/blogs.db")
+    add_news()
     app.run(port=8080, host='127.0.0.1')
